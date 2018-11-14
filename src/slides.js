a354679aa25dc076d6a2be7457cb77de09d629d9
Cesar Carruitero <cesar@mozilla.pe>
2018-11-14 00:20:17 -0500

    adding more slides content

--- src/slides.js
+++ src/slides.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { TitleSlide, ContentSlide } from 'react-presents'
+import { TitleSlide, ContentSlide, Step, Code } from 'react-presents'
+import './App.css';
+import manifestCode from './snippets/manifest.json'
 
 const slides = [];
 
@@ -11,7 +13,99 @@ slides.push(() => (
 
 slides.push(() => (
   <ContentSlide>
+    <h1>Web Extensions</h1>
+    <ul>
+      <Step index={1}>
+        <li>Personalize your browser experience</li>
+      </Step>
+      <Step index={2}>
+        <li>Extend browser functionality</li>
+      </Step>
+      <Step index={3}>
+        <li>Cross browser compatibility</li>
+      </Step>
+      <Step index={4}>
+        <li>Created using web-based technologies (HTML/CSS/JS)</li>
+      </Step>
+    </ul>
+  </ContentSlide>
+));
+
+slides.push(() => (
+  <ContentSlide>
     <h1>Anatomy of Web Extensions</h1>
+    <img
+      className='page-img'
+      alt=''
+      src='https://mdn.mozillademos.org/files/13669/webextension-anatomy.png'
+    />
+  </ContentSlide>
+));
+
+slides.push(() => (
+  <ContentSlide>
+    <h1>manifest.js</h1>
+    <Code
+      value={JSON.stringify(manifestCode, null, 2)}
+    />
+  </ContentSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>Content Scripts</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>Content Scripts</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>Background Scripts</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>BrowserAction API</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>pageAction API</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>Tabs API</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>web-ext</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>create-webextension</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <ContentSlide>
+    <h1>Resource</h1>
+    <ul>
+      <li><a href='https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions'>MDN - Web Extensions docs</a></li>
+      <li><a href='https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Examples'>Web Extensions examples</a></li>
+    </ul>
   </ContentSlide>
 ));
 
