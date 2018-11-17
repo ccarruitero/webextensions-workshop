772e7ab1e1ce11f3a0e4a6499bdf123896ef400e
Cesar Carruitero <cesar@mozilla.pe>
2018-11-17 02:20:20 -0500

    some additional content

--- src/slides.js
+++ src/slides.js
@@ -1,14 +1,63 @@
 import React from 'react'
 import { TitleSlide, ContentSlide, Step, Code } from 'react-presents'
 import './App.css';
-import manifestCode from './snippets/manifest.json'
+import manifestCode from './snippets/manifest.json';
 
 const slides = [];
 
 slides.push(() => (
   <TitleSlide>
     <h1>Building Web Extensions</h1>
+    <h2>Mozilla Perú</h2>
+    <h2>
+      <a href='https://github.com/mozillaperu'>
+        <i className='fa fa-github' />
+      </a>
+      <a href='https://twitter.com/mozillaperu'>
+        <i className='fa fa-twitter' />
+      </a>
+    </h2>
   </TitleSlide>
+));
+
+slides.push(() => (
+  <ContentSlide>
+    <h1>Who I am ?</h1>
+    <Step index={1}>
+      <h2>
+        César Carruitero
+        <a href='https://github.com/ccarruitero'>
+          <i className='fa fa-github'/>
+          ccarruitero
+        </a>
+      </h2>
+    </Step>
+    <ul>
+      <Step index={2}>
+        <li>Software developer for ~8 years </li>
+      </Step>
+      <Step index={3}>
+        <li>Mozillian, Mozilla volunteer </li>
+      </Step>
+      <Step index={4}>
+        <li>...</li>
+      </Step>
+    </ul>
+  </ContentSlide>
+));
+
+slides.push(() => (
+  <ContentSlide>
+    <h1>Mozilla</h1>
+    <img
+      className='full-img'
+      alt=''
+      src='https://www.mozilla.org/media/img/contribute/studentambassadors/hero.513535501a1e.jpg'
+    />
+    <p>
+      'https://www.mozilla.org/media/img/contribute/studentambassadors/hero.513535501a1e.jpg'
+    </p>
+  </ContentSlide>
 ));
 
 slides.push(() => (
@@ -16,15 +65,18 @@ slides.push(() => (
     <h1>Web Extensions</h1>
     <ul>
       <Step index={1}>
-        <li>Personalize your browser experience</li>
+        <h2>Addons</h2>
       </Step>
       <Step index={2}>
-        <li>Extend browser functionality</li>
+        <li>Personalize your browser experience</li>
       </Step>
       <Step index={3}>
-        <li>Cross browser compatibility</li>
+        <li>Extend browser functionality</li>
       </Step>
       <Step index={4}>
+        <li>Cross browser compatibility</li>
+      </Step>
+      <Step index={5}>
         <li>Created using web-based technologies (HTML/CSS/JS)</li>
       </Step>
     </ul>
@@ -39,12 +91,13 @@ slides.push(() => (
       alt=''
       src='https://mdn.mozillademos.org/files/13669/webextension-anatomy.png'
     />
+    <p>'https://mdn.mozillademos.org/files/13669/webextension-anatomy.png'</p>
   </ContentSlide>
 ));
 
 slides.push(() => (
   <ContentSlide>
-    <h1>manifest.js</h1>
+    <h1>manifest.json</h1>
     <Code
       value={JSON.stringify(manifestCode, null, 2)}
     />
@@ -58,20 +111,55 @@ slides.push(() => (
 ));
 
 slides.push(() => (
-  <TitleSlide>
-    <h1>Content Scripts</h1>
-  </TitleSlide>
+  <ContentSlide>
+    <h2>Content Scripts</h2>
+    <img
+      className='page-img'
+      src='http://localhost:5000/borderify.png'
+      alt=''
+    />
+  </ContentSlide>
 ));
 
 slides.push(() => (
   <TitleSlide>
-    <h1>Background Scripts</h1>
+    <h1>Javascript APIs</h1>
+    <Step index={1}>
+    <h2>for WebExtensions</h2>
+    <img
+      className='page-img'
+      src='http://localhost:5000/javascript-apis.png'
+      alt=''
+    />
+    </Step>
   </TitleSlide>
 ));
+slides.push(() => (
+  <ContentSlide>
+    <h1>
+      Javascript APIs for WebExtensions
+    </h1>
+    <ul>
+    <Step index={0}>
+      <li>some require request permissions</li>
+    </Step>
+    <Step index={1}>
+      <li>some return Promises</li>
+    </Step>
+    </ul>
+  </ContentSlide>
+));
 
+const browserApi = "a button in the browser's toolbar";
 slides.push(() => (
   <TitleSlide>
     <h1>BrowserAction API</h1>
+    <Step index={1}>
+    <li>{browserApi}</li>
+    </Step>
+    <Step index={2}>
+    <li>properties defined in manifest.json</li>
+    </Step>
   </TitleSlide>
 ));
 
@@ -89,6 +177,18 @@ slides.push(() => (
 
 slides.push(() => (
   <TitleSlide>
+    <h1>Background Scripts</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>Content Scripts</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
     <h1>web-ext</h1>
   </TitleSlide>
 ));
@@ -96,6 +196,12 @@ slides.push(() => (
 slides.push(() => (
   <TitleSlide>
     <h1>create-webextension</h1>
+  </TitleSlide>
+));
+
+slides.push(() => (
+  <TitleSlide>
+    <h1>webextension-polyfill</h1>
   </TitleSlide>
 ));
 
