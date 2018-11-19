30314b41d150904aa5d9fad31952893e4eb77e7a
Cesar Carruitero <cesar@mozilla.pe>
2018-11-19 13:22:03 -0500

    build

---
+++ precache-manifest.ddeae8b133382491fa2176338d84d580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "625fc87a05b45d647843c57ce68a933f",
+    "url": "/static/media/javascript-apis.625fc87a.png"
+  },
+  {
+    "revision": "c00c6711006edeb8e4a8ff917c14090c",
+    "url": "/static/media/borderify.c00c6711.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "105460565dbea010faff",
+    "url": "/static/js/main.10546056.chunk.js"
+  },
+  {
+    "revision": "0a803b7edacf3548544a",
+    "url": "/static/js/1.0a803b7e.chunk.js"
+  },
+  {
+    "revision": "105460565dbea010faff",
+    "url": "/static/css/main.a922826c.chunk.css"
+  },
+  {
+    "revision": "ce3d6d380563845523e6ade326c87290",
+    "url": "/index.html"
+  }
+];
