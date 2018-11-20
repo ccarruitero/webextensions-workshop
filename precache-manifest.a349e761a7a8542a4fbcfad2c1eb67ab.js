cbf6de7bba8f26f3b877476edcc4cb720fc334f0
César Carruitero <ccarruitero@protonmail.com>
2018-11-19 20:31:42 -0500

    update build

---
+++ precache-manifest.a349e761a7a8542a4fbcfad2c1eb67ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "651e893ba6e962e53921",
+    "url": "mozillaperu.github.io/webextensions-workshop/static/js/runtime~main.651e893b.js"
+  },
+  {
+    "revision": "2e9430083c9c5ca78d34",
+    "url": "mozillaperu.github.io/webextensions-workshop/static/js/main.2e943008.chunk.js"
+  },
+  {
+    "revision": "84fd8eba9ed30665a8c6",
+    "url": "mozillaperu.github.io/webextensions-workshop/static/js/1.84fd8eba.chunk.js"
+  },
+  {
+    "revision": "2e9430083c9c5ca78d34",
+    "url": "mozillaperu.github.io/webextensions-workshop/static/css/main.2ddc4bba.chunk.css"
+  },
+  {
+    "revision": "06dc9dfccd9f0dee7780c45ad8f9cf40",
+    "url": "mozillaperu.github.io/webextensions-workshop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63f00428d853c9a0ab0c24d95738fae8.js"
+  "mozillaperu.github.io/webextensions-workshop/precache-manifest.a349e761a7a8542a4fbcfad2c1eb67ab.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("mozillaperu.github.io/webextensions-workshop/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
